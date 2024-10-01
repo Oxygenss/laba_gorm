039ff87fc6996341ae075d1f6d45953895999833
oxygen- <dmizgeev@mail.ru>
2024-10-02 00:42:29 +0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Oxygenss/laba_gorm
+
+go 1.21.10
---
+++ internal/models/models.go
@@ -0,0 +1,9 @@
+package models
+
+type Students struct {
+	ID        uint
+	FIO       string
+	GradeBook string
+	Birthday  int64
+	StartDay  int64
+}
